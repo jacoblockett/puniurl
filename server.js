791843814d27d354e9c97462dc5f529f86b146e4
Jacob Lockett <jacobwyattlockett@gmail.com>
2019-12-29 13:34:45 -0500

    setting up for heroku basic

--- server.js
+++ server.js
@@ -10,28 +10,26 @@ const protocolReg = new RegExp(/^https?:\/\//, 'i')
 const domainName = 'https://puni-url.herokuapp.com/' //'puniurl.com/'
 let database, linkToGive
 
-console.log(process.env)
+if (database === undefined) {
+  const { TYPE, PROJECT_ID, PRIVATE_KEY_ID, PRIVATE_KEY, CLIENT_EMAIL, CLIENT_ID, AUTH_URI, TOKEN_URI, AUTH_PROVIDER_X509_CERT_URL, CLIENT_X509_CERT_URL, DATABASE_URL } = process.env
 
-// if (database === undefined) {
-//   const { TYPE, PROJECT_ID, PRIVATE_KEY_ID, PRIVATE_KEY, CLIENT_EMAIL, CLIENT_ID, AUTH_URI, TOKEN_URI, AUTH_PROVIDER_X509_CERT_URL, CLIENT_X509_CERT_URL, DATABASE_URL } = process.env
-
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       type: TYPE,
-//       project_id: PROJECT_ID,
-//       private_key_id: PRIVATE_KEY_ID,
-//       private_key: PRIVATE_KEY,
-//       client_email: CLIENT_EMAIL,
-//       client_id: CLIENT_ID,
-//       auth_uri: AUTH_URI,
-//       token_uri: TOKEN_URI,
-//       auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
-//       client_x509_cert_url: CLIENT_X509_CERT_URL,
-//     }),
-//     databaseURL: DATABASE_URL
-//   })
-//   database = admin.firestore()
-// }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: TYPE,
+      project_id: PROJECT_ID,
+      private_key_id: PRIVATE_KEY_ID,
+      private_key: PRIVATE_KEY,
+      client_email: CLIENT_EMAIL,
+      client_id: CLIENT_ID,
+      auth_uri: AUTH_URI,
+      token_uri: TOKEN_URI,
+      auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: CLIENT_X509_CERT_URL,
+    }),
+    databaseURL: DATABASE_URL
+  })
+  database = admin.firestore()
+}
 
 async function givePuni(col) {
   const puniurl = crs({type: 'url-safe', length: 8})
