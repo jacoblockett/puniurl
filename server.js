b39069744a53c5eeda301fc427ce78f7ec8ba520
Jacob Lockett <jacobwyattlockett@gmail.com>
2019-12-29 17:45:51 -0500

    launching final heroku

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ async function givePuni(col) {
 app.set('view engine', 'pug')
 app.set('port', 3000)
 
-app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}))
+app.use(helmet())
 app.use('/', express.json({limit: '1mb'}))
 app.use(express.static(__dirname + '/public'))
 
